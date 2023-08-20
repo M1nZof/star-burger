32e16169a005eef3f5465ea08708fff3d50277b9
M1nZof <maxykl38@gmail.com>
2023-08-20 16:57:36 +0300

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,7 +1,8 @@
-import json
-
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.http import JsonResponse
 from django.templatetags.static import static
+
+from phonenumber_field.validators import validate_international_phonenumber
 
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -65,6 +66,7 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     response = request.data
+
     try:
         products_from_response = response['products']
     except KeyError:
@@ -72,14 +74,42 @@ def register_order(request):
     if products_from_response is None:
         return Response({'error': 'products can not be null'}, status=status.HTTP_400_BAD_REQUEST)
     elif not isinstance(products_from_response, list):
-        return Response({'error': 'products should be presented as a list'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'products should be presented as a list'}, status=status.HTTP_406_NOT_ACCEPTABLE)
     elif len(products_from_response) == 0:
         return Response({'error': 'products list can not be empty'}, status=status.HTTP_400_BAD_REQUEST)
-    firstname, lastname = response['firstname'], response['lastname']
-    phonenumber, address = response['phonenumber'], response['address']
+
+    try:
+        firstname, lastname = response['firstname'], response['lastname']
+    except KeyError as e:
+        return Response({'error': f'{e} is a required field'}, status=status.HTTP_400_BAD_REQUEST)
+    if firstname is None:
+        return Response({'error': 'firstname can not be empty'}, status=status.HTTP_400_BAD_REQUEST)
+    elif lastname is None:
+        return Response({'error': 'lastname can not be empty'}, status=status.HTTP_400_BAD_REQUEST)
+    elif not isinstance(firstname, str):
+        return Response({'error': 'firstname should be str'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    elif not isinstance(lastname, str):
+        return Response({'error': 'lastname should be str'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    try:
+        phonenumber, address = response['phonenumber'], response['address']
+        validate_international_phonenumber(phonenumber)
+    except KeyError as e:
+        return Response({'error': f'{e} is a required field'}, status=status.HTTP_400_BAD_REQUEST)
+    except ValidationError:
+        return Response({'error': 'phonenumber is not correct, please make sure that you wrote correct number'},
+                        status=status.HTTP_406_NOT_ACCEPTABLE)
+    if phonenumber is None or len(phonenumber) < 1:
+        return Response({'error': 'phonenumber can not be empty'}, status=status.HTTP_400_BAD_REQUEST)
+    elif address is None:
+        return Response({'error': 'address can not be empty'}, status=status.HTTP_400_BAD_REQUEST)
+
     products = []
     for product in products_from_response:
-        products.append(Product.objects.get(pk=product['product']))
+        try:
+            products.append(Product.objects.get(pk=product['product']))
+        except ObjectDoesNotExist:
+            return Response({'error': 'product id does not exist'}, status=status.HTTP_400_BAD_REQUEST)
     order = Order.objects.create(
         first_name=firstname,
         last_name=lastname,
